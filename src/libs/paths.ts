964c36b0078adeb692821aac7bf224f7b19f0c4a
akirco <1833708553@qq.com>
2023-01-27 21:03:43 +0800

    rm extras

--- src/libs/paths.ts
+++ src/libs/paths.ts
@@ -1,17 +1,10 @@
 import { join, dirname } from "node:path";
 import { app } from "electron";
-import isDev from "electron-is-dev";
-import { rootPath } from "electron-root-path";
 
-const rootDirectory = app.getAppPath();
-let resources = "";
-if (isDev) {
-  resources = join(rootPath, "resources");
-  console.log(resources);
-} else {
-  resources = dirname(rootDirectory);
-  console.log(resources);
-}
+const rootPath = app.getAppPath();
+
+const resources = dirname(rootPath);
+
 export const rootDir = rootPath;
 export const execsPath = join(resources, "bin");
 export const modelsPath = join(resources, "models");
